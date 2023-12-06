b5bb20a7844bb23490615be9b2a0f2e2c6ea2d68
Andrew Orner <essbuh+git@gmail.com>
2023-12-06 07:22:03 -0500

    more efficient implementation of day5 pt2

--- 2023/Day5/src/main.rs
+++ 2023/Day5/src/main.rs
@@ -1,130 +1,127 @@
-use std::{collections::HashMap, str::Lines, sync::Arc};
-use core::cmp::min;
-use crossbeam::thread as cb_thread;
+use std::{str::Lines, time::SystemTime};
+
+#[derive(Debug, Copy, Clone)]
+struct Range {
+    start: i64,
+    end: i64,
+}
+impl Range {
+    fn contains_range(&self, range: &Range) -> bool {
+        self.start <= range.start
+            && self.end >= range.end
+    }
+}
 
 #[derive(Debug)]
-struct NumberMapping {
-    source_start: i64,
-    dest_start: i64,
-    range_size: i64
+struct MapConversion {
+    source: Range,
+    dest: Range,
 }
-impl NumberMapping {
-    fn map_value(&self, value: i64) -> Option<i64> {
-        if value >= self.source_start && value <= (self.source_start + self.range_size) {
-            let mapped_value = (value - self.source_start) + self.dest_start;
-            return Some(mapped_value);
+impl MapConversion {
+    fn convert_range(&self, range: &Range) -> Range {
+        if self.source.contains_range(range) {
+            let start = (range.start - self.source.start) + self.dest.start;
+            let end = (range.end - range.start) + start;
+            return Range { start, end };
         }
-
-        None
+        
+        return *range;
     }
 }
 
 #[derive(Debug)]
-struct NumberMap {
-    mappings: Vec<NumberMapping>
+struct AlmanacMap {
+    conversions: Vec<MapConversion>,
 }
-impl NumberMap {
-    fn new() -> NumberMap {
-        NumberMap { mappings : Vec::new() }
-    }
+impl AlmanacMap {
+    fn split_ranges(&self, range: &Range) -> Vec<Range> {
+        let mut queue : Vec<Range> = Vec::new();
+        queue.push(*range);
 
-    fn map_value(&self, value: i64) -> i64 {
-        for mapping in &self.mappings {
-            match mapping.map_value(value) {
-                Some(x) => { return x; },
-                None => ()
-            }
-        }
+        let mut result : Vec<Range> = Vec::new();
 
-        value
-    }
-}
-
-#[derive(Debug)]
-struct AlmanacEntry {
-    target_entry: String,
-    map: NumberMap,
-}
-
-#[derive(Debug)]
-struct SeedRange {
-    min: i64,
-    len: i64,
-}
-
-#[derive(Debug)]
-enum SeedType {
-    Seed(i64),
-    Range(SeedRange),
-}
-
-#[derive(Debug)]
-struct Almanac {
-    seeds: Vec<SeedType>,
-    maps: HashMap<String, AlmanacEntry>
-}
-impl Almanac {
-    fn get_seed_location(self: &Self, seed: i64) -> i64 {
-        let mut map_entry = self.maps.get("seed").expect("Almanac has no seed maps");
-        //println!("Mapping values from SEED map...");
-
-        let mut value : i64 = seed;
         loop {
-            // Map all values to next level of map
-            value = map_entry.map.map_value(value);
+            match queue.pop() {
+                Some(r) => {
+                    let mut found_match = false;
+                    for conversion in &self.conversions {
+                        let start = r.start;
+                        let end = r.end;
 
-            // Increment map level
-            match self.maps.get(&map_entry.target_entry) {
-                Some(entry) => { map_entry = entry; /*println!("Next map type: {}...", &map_entry.target_entry);*/ },
+                        if start < conversion.source.start {
+                            if end < conversion.source.start {
+                                // fully before this entry
+                                result.push(Range { start, end });
+                            } else {
+                                result.push(Range { start, end: conversion.source.start - 1 });
+                                
+                                // process next time
+                                queue.push(Range { start: conversion.source.start, end });
+                            }                            
+
+                            found_match = true;
+                            break;
+                        } else if start <= conversion.source.end {
+                            if end <= conversion.source.end {
+                                // fully inside this entry
+                                result.push(Range { start, end });
+                            } else {
+                                result.push(Range { start, end: conversion.source.end });
+
+                                // process next time
+                                queue.push(Range { start: conversion.source.end + 1, end });
+                            }
+                            found_match = true;
+                            break;
+                        }
+                    }
+
+                    if !found_match {
+                        // No conversion found, just map the range directly
+                        result.push(*range);
+                    }
+                 }
                 None => { break; }
             }
         }
 
-        value
+        result
     }
 
-    fn get_lowest_seed_location(self: &Self, seed: &SeedType) -> i64 {
-        println!("Checking lowest seed: {:?}", seed);
+    fn convert_range(&self, range: &Range) -> Range {
+        self.conversions.iter()
+            .find(|conv| conv.source.contains_range(range))
+            .map(|conv| conv.convert_range(range))
+            .unwrap_or(*range)
+    }
 
-        let location : i64;
+    fn map_values(&self, value: &Vec<Range>) -> Vec<Range> {
+        value.iter()
+            .map(|range| self.split_ranges(&range))
+            .flatten()
+            .map(|range| self.convert_range(&range))
+            .collect()
+    }
+}
 
-        match seed {
-            SeedType::Range(range) => { 
-                //locations = self.get_seed_locations(range.min, range.len); 
-                location = (range.min..(range.min + range.len))
-                    .map(|x| self.get_seed_location(x))
-                    .fold(i64::MAX, |acc, val| min(acc, val));
-            },
-            SeedType::Seed(value) => { 
-                //locations = self.get_seed_locations(*value, 1); 
-                location = self.get_seed_location(*value);
-            }
+#[derive(Debug)]
+struct Almanac {
+    seeds: Vec<Range>,
+    maps: Vec<AlmanacMap>
+}
+impl Almanac {
+    fn get_lowest_seed_location(self: &Self) -> i64 {
+        let mut locations = self.seeds.to_vec();
+        
+        for map in &self.maps {
+            locations = map.map_values(&locations);
         }
 
-        println!("Seed {:?} lowest location: {location}", seed);
+        locations.sort_by(|a, b| a.start.cmp(&b.start));
 
-        location
-    }
+        assert!(!locations.is_empty());
 
-    fn get_lowest_location(self: &Self) -> i64 {
-        let self_arc = Arc::new(self);
-
-        cb_thread::scope(|s| {
-            let mut threads : Vec<cb_thread::ScopedJoinHandle<i64>> = Vec::new();
-
-            for seed in &self.seeds {
-                let seed_arc = Arc::new(seed);
-                let self_clone = self_arc.clone();
-
-                threads.push(s.spawn(move |_| {
-                    self_clone.get_lowest_seed_location(&seed_arc)
-                }));
-            }
-
-            threads.into_iter()
-                .map(|t| t.join().unwrap() )
-                .fold(i64::MAX, |acc, val| min(acc, val))
-        }).unwrap()        
+        locations[0].start
     }
 }
 
@@ -137,36 +134,38 @@ fn parse_seeds(input: &mut Lines, almanac: &mut Almanac, use_seed_ranges: bool) 
         // Must be a pair of seeds
         assert_eq!(seeds.len() % 2, 0);
         for i in (0..seeds.len()).step_by(2) {
-            let range = SeedRange { min: seeds[i], len: seeds[i+1] };
-            almanac.seeds.push(SeedType::Range(range));
+            let range = Range { start: seeds[i], end: (seeds[i] + seeds[i+1] - 1) };
+            almanac.seeds.push(range);
         }
     } else {
-        almanac.seeds = seeds.iter().map(|x| SeedType::Seed(*x)).collect();
+        almanac.seeds = seeds.iter().map(|x| Range { start: *x, end: *x } ).collect();
     }
 
+    //almanac.seeds.sort_by(|a, b| a.start.cmp(&b.start));
+    
     let next_line = input.next().expect("Incorrect input format");
     assert!(next_line.is_empty());
 }
 
-fn parse_range(input: &str) -> NumberMapping {
+fn parse_map_line(input: &str) -> MapConversion {
     let numbers : Vec<i64> = input.split(' ').map(|x| x.parse::<i64>().expect("Incorrect input format")).collect();
     assert_eq!(numbers.len(), 3);
     
-    NumberMapping {
-        dest_start: numbers[0],
-        source_start: numbers[1],
-        range_size: numbers[2]
+    MapConversion {
+        source: Range { start: numbers[1], end: numbers[1] + numbers[2] - 1 },
+        dest: Range { start: numbers[0], end: numbers[0] + numbers[2] - 1 }
     }
 }
 
-fn parse_map_ranges(input: &mut Lines) -> NumberMap {
-    let mut map = NumberMap::new();
+fn parse_map_ranges(input: &mut Lines) -> Vec<MapConversion> {
+    let mut map = Vec::new();
+    map.reserve(14);
 
     loop {
         match input.next() {
             Some(line) => {
                 if !line.is_empty() {
-                    map.mappings.push(parse_range(line));
+                    map.push(parse_map_line(line));
                 } else {
                     break;                    
                 }
@@ -175,40 +174,32 @@ fn parse_map_ranges(input: &mut Lines) -> NumberMap {
         }
     }
 
+    map.sort_by(|a, b| a.source.start.cmp(&b.source.start));
+
     map
 }
 
-fn parse_map(header: &str, input: &mut Lines, almanac: &mut Almanac) {
-    let (map_type, map_text) = header.split_once(' ').expect("Incorrect input format");
-    assert_eq!(map_text, "map:");
-    let map_types : Vec<&str> = map_type.split('-').collect();
-    assert_eq!(map_types.len(), 3);
-
-    let from_type = map_types[0];
-    let to_type = map_types[2];
-
-    let entry = AlmanacEntry {
-        target_entry: to_type.to_string(),
-        map: parse_map_ranges(input)
-    };
-    
-    almanac.maps.insert(from_type.to_string(), entry);
+fn parse_map(input: &mut Lines, almanac: &mut Almanac) {
+    let map = AlmanacMap { conversions: parse_map_ranges(input) };
+    almanac.maps.push(map);
 }
 
 fn try_parse_map(input: &mut Lines, almanac: &mut Almanac) -> bool {
-
+    let now = SystemTime::now();
     let map_header = input.next();
     match map_header {
-        Some(header) => { parse_map(&header, input, almanac); return true; },
+        Some(_) => { parse_map(input, almanac); println!("Map parse took {} ms ", (now.elapsed().unwrap().as_micros() as f32 / 1000.0)); return true; },
         None => { return false; }
     }
 }
 
 fn parse_almanac(input: &mut Lines, use_seed_ranges: bool) -> Almanac {
-    let mut almanac = Almanac { seeds: Vec::new(), maps: HashMap::new() };
+    let mut almanac = Almanac { seeds: Vec::new(), maps: Vec::new() };
 
+    let now = SystemTime::now();
     parse_seeds(input, &mut almanac, use_seed_ranges);
-
+    println!("Seed parse took {} ms ", (now.elapsed().unwrap().as_micros() as f32 / 1000.0));
+    
     // parse all available maps we can find
     let mut result = true;
     while result {
@@ -218,30 +209,21 @@ fn parse_almanac(input: &mut Lines, use_seed_ranges: bool) -> Almanac {
     almanac
 }
 
+fn run_test(input: &str, use_seed_range: bool) {
+    let mut lines = input.lines();    
+
+    let now = SystemTime::now();        
+    let almanac = parse_almanac(&mut lines, use_seed_range);
+    println!("Finished parsing almanac in {} ms", (now.elapsed().unwrap().as_micros() as f32 / 1000.0));
+
+    let now = SystemTime::now();
+    let lowest_loc = almanac.get_lowest_seed_location();
+    println!("Lowest Location (range={use_seed_range}): {lowest_loc} | time spent: {} ms", (now.elapsed().unwrap().as_micros() as f32 / 1000.0));   
+}
+
 fn main() {
     let input = include_str!("input.txt");
     
-    {
-        let use_seed_range = false;
-        let mut lines = input.lines();    
-        let almanac = parse_almanac(&mut lines, use_seed_range);
-        println!("Finished parsing almanac.");
-    
-        //println!("Almanac: {:#?}", almanac);  
-    
-        let lowest_loc = almanac.get_lowest_location();
-        println!("Lowest Location (range={use_seed_range}): {lowest_loc}");   
-    }
-
-    {
-        let use_seed_range = true;
-        let mut lines = input.lines();    
-        let almanac = parse_almanac(&mut lines, use_seed_range);
-        println!("Finished parsing almanac.");
-    
-        //println!("Almanac: {:#?}", almanac);  
-    
-        let lowest_loc = almanac.get_lowest_location();
-        println!("Lowest Location (range={use_seed_range}): {lowest_loc}");   
-    }
+    run_test(input, false);
+    run_test(input, true);
 }
