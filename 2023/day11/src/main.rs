03331eb61d2b0982dcb1ade54a566ba1dae782da
Andrew Orner <essbuh+git@gmail.com>
2023-12-11 09:03:30 -0500

    "proper" A* implementation

--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{collections::{HashMap, BinaryHeap, HashSet}, cmp::Ordering};
 
 const EXPANSION_CHAR : char = 'X';
 
@@ -22,7 +22,7 @@ fn is_search_move_allowed(delta_x: i32, delta_y: i32, allow_diagonal: bool) -> b
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct Point {
     x: usize,
     y: usize,
@@ -33,10 +33,21 @@ impl Point {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct SearchCandidate {
     point: Point,
     dist: usize,
+}
+impl Ord for SearchCandidate {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.dist.cmp(&self.dist)
+            .then_with(|| other.point.cmp(&self.point))
+    }
+}
+impl PartialOrd for SearchCandidate {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 #[derive(Debug)]
@@ -89,17 +100,7 @@ impl GalaxyMap {
     }
 
     fn get_distance(self: &Self, point_a: &Point, point_b: &Point) -> usize {
-        let mut distance: usize = 0;
-
-        let range_y = if point_a.y < point_b.y { point_a.y..(point_b.y+1) } else { point_b.y..(point_a.y+1) };
-        for y in range_y {
-            let range_x = if point_a.x < point_b.x { point_a.x..(point_b.x+1) } else { point_b.x..(point_a.x+1) };
-            for x in range_x {
-                distance = distance + self.get_cell_cost(x, y);
-            }
-        }
-
-        distance
+        point_a.x.abs_diff(point_b.x) + point_b.y.abs_diff(point_b.y)
     }
 
     fn get_cell_cost(self: &Self, x: usize, y: usize) -> usize {
@@ -120,7 +121,7 @@ impl GalaxyMap {
                 let candidate_y = (point.y as i32) + y;
                 let candidate_point = Point { x: candidate_x as usize, y: candidate_y as usize };
                 if &candidate_point != point && self.is_valid_point(&candidate_point) {
-                    let candidate = SearchCandidate { point: candidate_point, dist: self.get_distance(&candidate_point, &dest) };
+                    let candidate = SearchCandidate { point: candidate_point, dist: self.get_distance(&candidate_point, dest) };
                     out_candidates.push(candidate);
                 }
             }
@@ -131,48 +132,68 @@ impl GalaxyMap {
         let point_a = &self.galaxies[galaxy_a];
         let point_b = &self.galaxies[galaxy_b];
 
-        let mut visited_nodes: HashSet<Point> = HashSet::new();
-        let mut search_stack : Vec<Point> = Vec::new();
-        let mut candidates : Vec<SearchCandidate> = Vec::new();
+        let mut open_set = BinaryHeap::new();
+        let mut open_set_hash = HashSet::new();
         let mut found_goal = false;
 
-        candidates.push(SearchCandidate { point: *point_a, dist: self.get_distance(&point_a, &point_b) });
+        let mut g_scores : HashMap<Point, usize> = HashMap::new();
+        g_scores.insert(*point_a, 0);
+
+        let mut f_scores : HashMap<Point, usize> = HashMap::new();
+        f_scores.insert(*point_a, self.get_distance(point_a, point_b));
+
+        let mut came_from : HashMap<Point, Point> = HashMap::new();
+
+        open_set.push(SearchCandidate{ point: *point_a, dist: 0 });
+        open_set_hash.insert(*point_a);
 
         loop {
-            if candidates.is_empty() {
+            if open_set.is_empty() {
                 break;
             }
 
-            let search_point = candidates.pop().unwrap().point;
-            if visited_nodes.contains(&search_point) {
-                continue;
-            }
-
-            visited_nodes.insert(search_point);
-
-            if &search_point == point_b {
-                search_stack.push(search_point);
+            let search_point = open_set.pop().unwrap();
+            if &search_point.point == point_b {
                 found_goal = true;
                 break;
-            }            
+            }
+            open_set_hash.remove(&search_point.point);
 
-            let mut search_candidates : Vec<SearchCandidate> = Vec::new();
-            self.get_search_candidates(&search_point, &point_b, &mut search_candidates);
+            let mut neighbors : Vec<SearchCandidate> = Vec::new();
+            self.get_search_candidates(&search_point.point, &point_b, &mut neighbors);
 
-            if !search_candidates.is_empty() {
-                search_stack.push(search_point);
-                search_candidates.sort_by(|a, b| b.dist.cmp(&a.dist));
-                candidates.append(&mut search_candidates);
+            for neighbor in neighbors {
+                let tentative_gscore = g_scores[&search_point.point] + self.get_cell_cost(neighbor.point.x, neighbor.point.y);
+                if tentative_gscore < *g_scores.entry(neighbor.point).or_insert(usize::MAX) {
+                    // This path to neighbor is better than any previous
+                    came_from.insert(neighbor.point, search_point.point);
+                    g_scores.insert(neighbor.point, tentative_gscore);
+                    f_scores.insert(neighbor.point, tentative_gscore + neighbor.dist);
+
+                    if !open_set_hash.contains(&neighbor.point) {
+                        open_set_hash.insert(neighbor.point.clone());
+                        open_set.push(neighbor);
+                    }
+                }
             }
         }
 
         assert_eq!(found_goal, true);
-        let first_item = search_stack.remove(0);
-        assert_eq!(&first_item, point_a);
 
-        let shortest_path = search_stack.iter().map(|p| self.get_cell_cost(p.x, p.y)).sum();
-        //println!("Shortest path between galaxies {galaxy_a} and {galaxy_b} is {shortest_path}");
-        shortest_path
+        let mut path : Vec<&Point> = vec![ point_b ];
+        loop {
+            match came_from.get(path.last().unwrap()) {
+                Some(p) => { path.push(p); },
+                None => { break; }
+            }
+        }
+
+        let last_point = path.pop().unwrap(); // Remove the start point
+        assert_eq!(last_point, point_a);
+
+        let total_cost = path.iter().map(|x| self.get_cell_cost(x.x, x.y)).sum();
+        //println!("Shortest path between galaxies {galaxy_a} and {galaxy_b} is {total_cost}");
+        total_cost
     }
 
     fn get_shortest_paths(self: &Self) -> usize {
