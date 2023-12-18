ff7596e039658d6f441268df7218aabc8d9e4475
Andrew Orner <essbuh+git@gmail.com>
2023-12-18 10:01:43 -0500

    day 18, optimized

--- 2023/day18/src/main.rs
+++ 2023/day18/src/main.rs
@@ -1,10 +1,4 @@
-use std::cmp::{min, max};
-
 type Point = (i32, i32);
-
-const CHAR_FILLED : char = '#';
-const CHAR_UP : char = '^';
-const CHAR_DOWN : char = 'v';
 
 fn hex_to_dec(hex: &[char]) -> u32 {
     let mut result = 0;
@@ -31,155 +25,62 @@ struct DigPlan {
 }
 impl DigPlan {
     fn run_instructions(self: &Self, map: &mut DigMap) {    
-        let mut pos = map.ensure_capacity(&self.instructions);
-
-        map.dig_hole(&pos, CHAR_FILLED);
+        let mut pos = (0,0);
 
         for instruction in &self.instructions {
             map.run_instruction(&mut pos, instruction);
         }
+
+        assert_eq!(pos, (0,0));
     }
 }
 struct DigMap
 {    
-    map: Vec<Vec<(i32, char)>>,
+    map: Vec<Point>,
 }
 impl DigMap {
-    fn ensure_capacity(self: &mut Self, instructions: &Vec<DigInstruction>) -> Point {
-        let mut point : Point = (0, 0);
-        let mut point_min : Point = (0, 0);
-        let mut point_max : Point = (0, 0);
-        for instruction in instructions {
-            match instruction.dir {
-                'R' => { 
-                    point.0 += instruction.count as i32; 
-                    point_min.0 = min(point_min.0, point.0);
-                    point_max.0 = max(point_max.0, point.0);
-                },
-                'L' => { 
-                    point.0 -= instruction.count as i32; 
-                    point_min.0 = min(point_min.0, point.0);
-                    point_max.0 = max(point_max.0, point.0);
-                },
-                'U' => { 
-                    point.1 -= instruction.count as i32;
-                    point_min.1 = min(point_min.1, point.1);
-                    point_max.1 = max(point_max.1, point.1);
-                 },
-                'D' => { 
-                    point.1 += instruction.count as i32;
-                    point_min.1 = min(point_min.1, point.1);
-                    point_max.1 = max(point_max.1, point.1);
-                 },
-                _ => {},
-            }
-        }
-
-        let bounds = (point_max.0 - point_min.0, point_max.1 - point_min.1);
-        self.map.resize_with((bounds.1 + 1) as usize, || Vec::new());
-
-        (-point_min.0, -point_min.1)
-    }
-
-    fn dig_hole(self: &mut Self, pos: &Point, val: char) {
-        let row = &mut self.map[pos.1 as usize];
-
-        let mut index = 0;
-        for (i, col) in row.iter_mut().enumerate() {
-            if col.0 == pos.0 {
-                if col.1 == CHAR_FILLED {
-                    col.1 = val;
-                }
-                return;
-            }
-
-            if col.0 > pos.0 {
-                break;
-            }
-
-            index = i + 1;
-        }
-
-        row.insert(index, (pos.0, val));
+    fn dig_hole(self: &mut Self, pos: &Point) {
+        self.map.push(*pos);
     }
 
     fn run_instruction(self: &mut Self, pos: &mut Point, instruction: &DigInstruction) {        
         match instruction.dir {
-            'R' => {
-                pos.0 += instruction.count as i32;
-                self.dig_hole(&(pos.0 - 1, pos.1), CHAR_FILLED);
-            },
-            'L' => {
-                pos.0 -= instruction.count as i32;
-                self.dig_hole(&(pos.0 + instruction.count as i32 - 1, pos.1), CHAR_FILLED);
-            },
-            'U' => {
-                self.dig_hole(&pos, CHAR_UP);
-                for _ in 0..instruction.count {
-                    pos.1 -= 1;
-                    self.dig_hole(&pos, CHAR_UP);
-                }
-            },
-            'D' => {
-                self.dig_hole(&pos, CHAR_DOWN);
-                for _ in 0..instruction.count {
-                    pos.1 += 1;
-                    self.dig_hole(&pos, CHAR_DOWN);
-                }
-            }
+            'R' => { pos.0 += instruction.count as i32; },
+            'L' => { pos.0 -= instruction.count as i32; },
+            'U' => { pos.1 -= instruction.count as i32; },
+            'D' => { pos.1 += instruction.count as i32; }
             _ => { panic!("Unknown instruction"); }
         }
+        self.dig_hole(pos);
     }
 
-    fn get_fill_size(self: &Self) -> usize {
-        let mut fill_size : usize = 0;
+    fn get_fill_size(self: &Self) -> i64 {
+        let mut area : i64 = 0;
+        let mut perimeter : i32 = 0;
 
-        for (_i, row) in self.map.iter().enumerate() {
-            let mut last_x = -1;
+        let map = &self.map;
 
-            let mut row_size = 0;
-            for col in row.iter() {
-                match col.1 {
-                    CHAR_UP => {
-                        row_size += 1;
-                        last_x = col.0;
-                    },
-                    CHAR_DOWN => {
-                        let fill_count = col.0 - last_x;
-                        row_size += fill_count as usize;
-                        last_x = col.0;
-                    },
-                    CHAR_FILLED => { 
-                        let fill_count = col.0 - last_x;
-                        row_size += fill_count as usize;
-                        last_x = col.0;
-                    }
-                    _ => {}
-                }
-            }
+        for i in 0..map.len()  {
+            let next_i = if i == map.len()-1 { 0 } else { i+1 };
 
-            fill_size += row_size;
+            let pt_a = map[i];
+            let pt_b = map[next_i];
+
+            let det = (pt_a.0 as i64 * pt_b.1 as i64) - (pt_b.0 as i64 * pt_a.1 as i64);
+            area += det;
+
+            perimeter += (pt_b.0 - pt_a.0).abs();
+            perimeter += (pt_b.1 - pt_a.1).abs();
         }
 
-        fill_size
-    }
-
-    fn print(self: &mut Self) {
-        /*println!(" -------- ");
-        println!(" ");
-
-        for line in &self.map {
-            let chars : Vec<char> = line.iter().map(|c| if c == &'.' { *c } else { '#' }).collect();
-            let s : String = chars.iter().collect();
-            println!("{s}");
-        }
-    
-        println!(" ");*/
+        let numerator = (area.abs() + perimeter as i64) as f64;
+        let result = (numerator / 2.0).ceil() as i64 + 1;
+        result
     }
 }
 
 fn parse_instruction(line: &str, color_is_instruction: bool) -> DigInstruction {
-    let parts : Vec<&str> = line.split(' ').collect();    
+    let parts : Vec<&str> = line.split(' ').collect();   
 
     if color_is_instruction {
         let color : Vec<char> = parts[2][2..8].to_string().chars().collect();
@@ -210,15 +111,13 @@ fn parse_input(input: &str, color_is_instruction: bool) -> DigPlan {
     map
 }
 
-fn run_part(input: &str, color_is_instruction: bool) -> usize {
+fn run_part(input: &str, color_is_instruction: bool) -> i64 {
     let plan = parse_input(input, color_is_instruction);
     let mut map = DigMap { map: Vec::new() };
     
     plan.run_instructions(&mut map);
-    map.print();
-    let result = map.get_fill_size();
 
-    result
+    map.get_fill_size()
 }
 
 fn part_1()
